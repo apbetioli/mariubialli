ab6130af8f5496f57eb313eb60b3bd6aeb804eab
Alexandre Betioli <apbetioli@gmail.com>
2020-10-19 21:42:19 -0300

    Extract Form Component

---
+++ components/Form.js
@@ -0,0 +1,78 @@
+import { Button, TextField } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import theme from "assets/js/theme";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Api from "services/apis";
+
+const MyTextField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "black",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "black",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: theme.palette.error.main,
+      },
+      "&:hover fieldset": {
+        borderColor: "gray",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "black",
+      },
+    },
+  },
+})(TextField);
+
+export default function Form(props) {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    let form = {
+      email,
+      tag: "TESTE",
+    };
+
+    try {
+      await Api.post("/subscribe", form);
+
+      router.push("/obrigado?email=" + email);
+    } catch (error) {
+      console.log(error);
+      alert("Ocorreu um erro ao se inscrever. Tente novamente mais tarde.");
+    }
+  }
+
+  return (
+    <form onSubmit={onSubmit.bind(this)}>
+      <MyTextField
+        id="filled-full-width"
+        label="Email"
+        type="email"
+        placeholder="Digite o seu melhor email..."
+        fullWidth
+        required
+        margin="normal"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange.bind(this)}
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Quero receber as apostilas em meu email
+      </Button>
+    </form>
+  );
+}
--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,8 @@
-import { Button, Grid, TextField } from "@material-ui/core";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import classnames from "classnames";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import theme from "assets/js/theme";
-import Api from "services/apis";
+import React from "react";
+import Form from "./Form";
 
 const backgroundImage = require("assets/img/hero.jpg");
 
@@ -38,89 +36,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const HeroTextField = withStyles({
-  root: {
-    "& label.Mui-focused": {
-      color: "black",
-    },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: "black",
-    },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: theme.palette.error.main,
-      },
-      "&:hover fieldset": {
-        borderColor: "gray",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "black",
-      },
-    },
-  },
-})(TextField);
-
 export default function Hero(props) {
-  const router = useRouter();
   const classes = useStyles();
-  const [email, setEmail] = useState("");
-
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    let form = {
-      email,
-      tag: "TESTE",
-    };
-
-    try {
-      console.log(process.env.REACT_APP_API_URL);
-      await Api.post("/subscribe", form);
-
-      router.push("/obrigado?email=" + email);
-
-    } catch (error) {
-      console.log(error);
-      alert("Ocorreu um erro ao se inscrever. Tente novamente mais tarde.");
-    }
-  }
 
   return (
-    <form onSubmit={onSubmit.bind(this)}>
-      <div className={classnames(classes.hero, "hero")}>
-        <Grid className={classes.grid} container spacing={0}>
-          <Grid item md={3}></Grid>
-          <Grid className={classes.centered} item md={6}>
-            <h1 className={classes.title}>Apostilas gratuitas toda semana</h1>
-            <h4 className={classes.subtitle}>
-              Deixe seu email e receba semanalmente as apostilas de moldes de
-              feltro que eu faço nos meus vídeos do youtube. E o melhor: são
-              totalmente gratuitas.
-            </h4>
-            <HeroTextField
-              id="filled-full-width"
-              label="Email"
-              type="email"
-              placeholder="Digite o seu melhor email..."
-              fullWidth
-              required
-              margin="normal"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="outlined"
-              value={email}
-              onChange={handleEmailChange.bind(this)}
-            />
-            <Button variant="contained" color="primary" type="submit">
-              Quero receber as apostilas em meu email
-            </Button>
-          </Grid>
+    <div className={classnames(classes.hero, "hero")}>
+      <Grid className={classes.grid} container spacing={0}>
+        <Grid item md={3}></Grid>
+        <Grid className={classes.centered} item md={6}>
+          <h1 className={classes.title}>Apostilas gratuitas toda semana</h1>
+          <h4 className={classes.subtitle}>
+            Deixe seu email e receba semanalmente as apostilas de moldes de
+            feltro que eu faço nos meus vídeos do youtube. E o melhor: são
+            totalmente gratuitas.
+          </h4>
+          <Form />
         </Grid>
-      </div>
-    </form>
+      </Grid>
+    </div>
   );
 }
