a1e4e889164bddaf87c470566f31fe47fb95b341
Alexandre Betioli <apbetioli@gmail.com>
2020-10-20 22:34:24 -0300

    Extract Hero content

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,6 @@ import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import classnames from "classnames";
 import React from "react";
-import Form from "./Form";
 
 const backgroundImage = require("assets/img/hero.jpg");
 
@@ -44,13 +43,7 @@ export default function Hero(props) {
       <Grid className={classes.grid} container spacing={0}>
         <Grid item md={3}></Grid>
         <Grid className={classes.centered} item md={6}>
-          <h1 className={classes.title}>Apostilas gratuitas toda semana</h1>
-          <h4 className={classes.subtitle}>
-            Deixe seu email e receba semanalmente as apostilas de moldes de
-            feltro que eu faço nos meus vídeos do youtube. E o melhor: são
-            totalmente gratuitas.
-          </h4>
-          <Form />
+          {props.children}
         </Grid>
       </Grid>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import { makeStyles } from "@material-ui/core";
+import Form from "components/Form";
 import Hero from "components/Hero";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: theme.palette.primary.main,
+    fontSize: "3.3125rem",
+    lineHeight: "1.5em",
+  },
+  subtitle: {
+    color: theme.palette.primary.main,
+    fontSize: "1.125rem",
+    lineHeight: "1.5em",
+  },
+}));
 
 export default function Home(props) {
+  const classes = useStyles();
   return (
     <>
-      <Hero />
+      <Hero>
+        <h1 className={classes.title}>Apostilas gratuitas toda semana</h1>
+        <h4 className={classes.subtitle}>
+          Deixe seu email e receba semanalmente as apostilas de moldes de feltro
+          que eu faço nos meus vídeos do youtube. E o melhor: são totalmente
+          gratuitas.
+        </h4>
+        <Form />
+      </Hero>
     </>
   );
 }
