e3b7c7a4cc20a31d589b0dc7a267f2e6194bbc32
Alexandre Betioli <apbetioli@gmail.com>
2020-10-28 22:51:28 -0300

    Cleanup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
-const webpack = require("webpack");
 const path = require("path");
 
 module.exports = withPlugins([[withImages]], {
-  webpack(config, options) {
+  target: "serverless",
+  webpack(config) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
   },
