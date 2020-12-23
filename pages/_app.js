50986e393eb8e26fbf8c528cfb73a86838bd6a85
Alexandre Betioli <apbetioli@gmail.com>
2020-12-23 17:29:29 -0300

    SEO

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,15 @@ export default function App({ Component, pageProps }) {
     <React.Fragment>
       <Head>
         <title>{title + "Mari Ubialli - Artesanato Criativo"}</title>
+        <meta
+          name="description"
+          content="Aprenda comigo tudo sobre artesanato em feltro e criação de apostilas de moldes. Baixe apostilas de moldes gratuitas prontas para confecção."
+        />
+        <meta
+          name="keywords"
+          content="feltro, artesanato, costura, ponto caseado, ponto reto, ponto alinhavo, passo a passo, diy, moldes gratuitos, apostilas gratuitas"
+        />
+        <meta name="robots" content="index,follow"></meta>
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
