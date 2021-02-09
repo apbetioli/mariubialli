8295d7af05224786d653d86e6c91a686da34d1a6
Alexandre Betioli <apbetioli@gmail.com>
2021-02-08 22:06:03 -0300

    Change whatsapp for email

--- pages/joiasraras/confirmado.js
+++ pages/joiasraras/confirmado.js
@@ -1,5 +1,4 @@
 import React from "react";
-import BotaoWhats from "../../components/BotaoWhats";
 import Layout from "../../components/Layout";
 import Obrigado from "../../components/Obrigado";
 
@@ -12,8 +11,7 @@ export default function JoiasRarasConfirmado(props) {
                 text="" >
                 <p>Você receberá em até 10 minutos um email da hotmart com o acesso ao curso.</p>
                 <p>Após este tempo se não receber, veja se ele pode ter caído na pasta de spam.</p>
-                <p>Qualquer dúvida entre em contato pelo WhatsApp clicando no botão abaixo.</p>
-                <BotaoWhats float={false} message="Oi, estou com dúvidas no acesso ao curso joias raras." />
+                <p>Qualquer dúvida entre em contato pelo email contato@mariubialli.com</p>
             </Obrigado>
         </Layout >
     );
