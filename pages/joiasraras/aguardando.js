42f97331a1ad67f2aa3e2197842d2a0aa982bce8
Alexandre Betioli <apbetioli@gmail.com>
2021-02-10 15:34:49 -0300

    Improve information

--- pages/joiasraras/aguardando.js
+++ pages/joiasraras/aguardando.js
@@ -8,9 +8,12 @@ export default function JoiasRarasAguardando(props) {
         <Layout {...props}>
             <Obrigado
                 social={false}
-                subtitle="Aguardando confirmação!"
+                subtitle="Leia com atenção!"
                 text="" >
-                <p>Se você pagou por boleto você receberá o acesso ao curso assim que o pagamento for aprovado.</p>
+                <p>Muito obrigada por se inscrever no curso das bonecas Joias Raras.</p>
+                <p>Faça o pagamento ainda hoje para que o curso seja liberado o mais rápido possível.</p>
+                <p>Você receberá o acesso ao curso automaticamente no email cadastrado assim que o pagamento for aprovado.</p>
+                <p>A aprovação do pagamento pelo banco pode levar até 72 horas.</p>
                 <p>Qualquer dúvida entre em contato pelo WhatsApp clicando no botão abaixo, ou por email <a href="mailto:contato@mariubialli.com">contato@mariubialli.com</a></p>
                 <BotaoWhats float={false} message="Oi, estou aguardando confirmação do pagamento do curso joias raras e gostaria de tirar algumas dúvidas." />
             </Obrigado>
--- pages/joiasraras/confirmado.js
+++ pages/joiasraras/confirmado.js
@@ -9,9 +9,10 @@ export default function JoiasRarasConfirmado(props) {
                 social={false}
                 subtitle="Inscrição confirmada!"
                 text="" >
-                <p>Você receberá em até 10 minutos um email da hotmart com o acesso ao curso.</p>
+                <p>Você receberá em até 10 minutos um email da hotmart com o seu acesso ao curso.</p>
                 <p>Após este tempo se não receber, veja se ele pode ter caído na pasta de spam.</p>
-                <p>Qualquer dúvida entre em contato pelo email contato@mariubialli.com</p>
+                <p>Nos vemos no curso!</p>
+                <p>Qualquer dúvida entre em contato pelo email <a href="mailto:contato@mariubialli.com">contato@mariubialli.com</a></p>
             </Obrigado>
         </Layout >
     );
