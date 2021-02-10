50352c49ae1f47327f364585ddd1ab46ca6b9d85
Alexandre Betioli <apbetioli@gmail.com>
2021-02-10 15:40:03 -0300

    Add info

--- pages/joiasraras/aguardando.js
+++ pages/joiasraras/aguardando.js
@@ -10,8 +10,8 @@ export default function JoiasRarasAguardando(props) {
                 social={false}
                 subtitle="Leia com atenção!"
                 text="" >
-                <p>Muito obrigada por se inscrever no curso das bonecas Joias Raras.</p>
-                <p>Faça o pagamento ainda hoje para que o curso seja liberado o mais rápido possível.</p>
+                <p>Fico muito feliz que tenha se inscrito no curso das bonecas Joias Raras.</p>
+                <p>Para não perder sua vaga, faça o pagamento <b>ainda hoje</b> para que o curso seja liberado o mais rápido possível.</p>
                 <p>Você receberá o acesso ao curso automaticamente no email cadastrado assim que o pagamento for aprovado.</p>
                 <p>A aprovação do pagamento pelo banco pode levar até 72 horas.</p>
                 <p>Qualquer dúvida entre em contato pelo WhatsApp clicando no botão abaixo, ou por email <a href="mailto:contato@mariubialli.com">contato@mariubialli.com</a></p>
--- pages/joiasraras/confirmado.js
+++ pages/joiasraras/confirmado.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BotaoWhats from "../../components/BotaoWhats";
 import Layout from "../../components/Layout";
 import Obrigado from "../../components/Obrigado";
 
@@ -9,10 +10,11 @@ export default function JoiasRarasConfirmado(props) {
                 social={false}
                 subtitle="Inscrição confirmada!"
                 text="" >
-                <p>Você receberá em até 10 minutos um email da hotmart com o seu acesso ao curso.</p>
+                <p>Você receberá em até 10 minutos um email da <b>Hotmart</b> com o seu acesso ao curso.</p>
                 <p>Após este tempo se não receber, veja se ele pode ter caído na pasta de spam.</p>
                 <p>Nos vemos no curso!</p>
-                <p>Qualquer dúvida entre em contato pelo email <a href="mailto:contato@mariubialli.com">contato@mariubialli.com</a></p>
+                <p>Qualquer dúvida entre em contato pelo WhatsApp clicando no botão abaixo, ou por email <a href="mailto:contato@mariubialli.com">contato@mariubialli.com</a></p>
+                <BotaoWhats float={false} message="Oi, estou com dificuldades em acessar o curso das bonecas joias raras e gostaria de tirar algumas dúvidas." />
             </Obrigado>
         </Layout >
     );
