ecad17392e1b6a4c6a1574b3317eecce5e2a7f6f
Alexandre Betioli <apbetioli@gmail.com>
2021-02-19 01:07:40 -0300

    Check for transaction status

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -11,10 +11,16 @@ module.exports = async (req, res) => {
 
       const transactions = db.db(process.env.MONGO_DB).collection("transactions");
       const query = { email: data.email };
-      const doc = { $set: data }
-      const options = { upsert: true };
-      const upserted = await transactions.updateOne(query, doc, options);
-      res.send(upserted);
+      const t = await transactions.findOne(query);
+
+      if(t.status != "approved" || ["completed", "refunded", "chargeback", "dispute"].includes(data.status)) {
+        const doc = { $set: data }
+        const options = { upsert: true };
+        const upserted = await transactions.updateOne(query, doc, options);
+        res.send(upserted);
+      } else {
+        res.send(t);
+      }
 
     } else {
       const db = await client.connect();
