dc60e9726a17365febf53eeb83107d6696eb40cb
Alexandre Betioli <apbetioli@gmail.com>
2021-03-07 12:50:40 -0300

    Call to action on billet purchase

--- pages/joiasraras/aguardando.js
+++ pages/joiasraras/aguardando.js
@@ -21,11 +21,11 @@ export default function JoiasRarasAguardando(props) {
                 text=""
                 title={getTitle()} >
                 <p>Fico muito feliz que tenha se inscrito no curso das bonecas Joias Raras.</p>
-                <p>Para não perder sua vaga, faça o pagamento <b>ainda hoje</b> para que o curso seja liberado o mais rápido possível.</p>
+                <p>Faça o pagamento <b>ainda hoje</b> e <b>envie o comprovante</b> para o WhatsApp abaixo para que o curso seja liberado o mais rápido possível.</p>
                 <p>Você receberá o acesso ao curso automaticamente no email cadastrado assim que o pagamento for aprovado.</p>
                 <p>A aprovação do pagamento pelo banco pode levar até 72 horas.</p>
                 <p>Qualquer dúvida entre em contato pelo WhatsApp clicando no botão abaixo, ou por email <a href="mailto:contato@mariubialli.com">contato@mariubialli.com</a></p>
-                <BotaoWhats float={false} message="Oi, estou aguardando confirmação do pagamento do curso joias raras e gostaria de tirar algumas dúvidas." />
+                <BotaoWhats float={false} message="Oi, fiz a compra por boleto do Curso Bonecas Joias Raras..." />
             </Obrigado>
         </Layout >
     );
