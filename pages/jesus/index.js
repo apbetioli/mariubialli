8029daa38728912f0824a3fa34cdcfc0f618bba8
Alexandre Betioli <apbetioli@gmail.com>
2021-04-20 09:04:15 -0300

    Fix image typo

--- pages/jesus/index.js
+++ pages/jesus/index.js
@@ -279,7 +279,7 @@ function Preco() {
   const router = useRouter()
   let priceImg = require("assets/img/price27.png")
   if (router.query.off == "nxgx5qja")
-    priceImg = require("assets/img/price_discount_19.jpg")
+    priceImg = require("assets/img/price_discount_19.png")
 
   return (
     <section className={classes.sectionGreen}>
