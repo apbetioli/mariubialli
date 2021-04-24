448554fe3f91f97309df95fdefdf928b7a5fb804
Alexandre Betioli <apbetioli@gmail.com>
2021-04-24 00:27:37 -0300

    Add phases to sales recovery

--- pages/admin/recupera.js
+++ pages/admin/recupera.js
@@ -8,6 +8,9 @@ import {
     FormControlLabel,
     IconButton,
     makeStyles,
+    MenuItem,
+    Select,
+
     TextField
 } from "@material-ui/core"
 import Paper from '@material-ui/core/Paper'
@@ -19,7 +22,7 @@ import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import CloseIcon from '@material-ui/icons/Close'
 import Head from "next/head"
-import React, { useEffect } from "react"
+import React from "react"
 import ColorButton from "../../components/ColorButton"
 
 const API_URL = `${process.env.NEXT_PUBLIC_DOMAIN}/api/webhook/transaction`
@@ -31,6 +34,24 @@ const useStyles = makeStyles({
     table: {
         minWidth: 650,
     },
+    phase1: {
+        backgroundColor: "#FFB6AD"
+    },
+    phase2: {
+        backgroundColor: "#FDECB6"
+    },
+    phase3: {
+        backgroundColor: "lightblue"
+    },
+    converteu: {
+        backgroundColor: "lightgreen"
+    },
+    normal: {
+
+    },
+    inputNumber: {
+        width: 100
+    }
 })
 
 const post = async (transaction, method = 'POST') => {
@@ -51,15 +72,14 @@ export default function Recupera(props) {
     const [transactions, setTransactions] = React.useState(props.transactions)
     const [open, setOpen] = React.useState(false)
     const [selected, setSelected] = React.useState({})
-    let [obs, setObs] = React.useState('')
+    const [obs, setObs] = React.useState('')
 
     const handleChangeShowArchived = () => {
         setShowArchived(!showArchived)
     }
 
-    const handleChange = (prop) => (event) => {
+    const handleChangeObs = (event) => {
         setObs(event.target.value)
-        selected[prop] = event.target.value
     }
 
     const handleClickOpen = (transaction) => {
@@ -75,10 +95,11 @@ export default function Recupera(props) {
     const onSubmit = async (e) => {
         e.preventDefault()
 
-        const res = await post(selected)
-        console.log(res)
-
+        selected['obs'] = obs
         setOpen(false)
+
+        const res = post(selected)
+        console.log(res)
     }
 
     const handleArchive = async (transaction) => {
@@ -102,6 +123,20 @@ export default function Recupera(props) {
         }
     }
 
+    const handleChangePhase = async (event, newValue, transaction, index) => {
+
+        if (transaction.phase != newValue) {
+            transaction.phase = newValue
+            setSelected(transaction)
+
+            const res = await post(transaction)
+            console.log(res)
+
+            setTransactions(await list())
+        }
+        //post(transaction)
+    }
+
     return (
         <>
             <Head>
@@ -117,19 +152,17 @@ export default function Recupera(props) {
                 <DialogContent>
                     <form onSubmit={onSubmit.bind(this)}>
                         <TextField
-                            id="filled-full-width"
                             label="Obs"
                             type="text"
                             placeholder="..."
                             fullWidth
-                            required
                             margin="normal"
                             InputLabelProps={{
                                 shrink: true,
                             }}
                             variant="outlined"
                             value={obs}
-                            onChange={handleChange("obs")}
+                            onChange={handleChangeObs}
                         />
                         <ColorButton
                             variant="contained"
@@ -164,30 +197,47 @@ export default function Recupera(props) {
                                 <TableCell>Obs</TableCell>
                                 <TableCell>Nome</TableCell>
                                 <TableCell>Email</TableCell>
-                                <TableCell>Celular</TableCell>
                                 <TableCell>Celular checkout</TableCell>
+                                <TableCell>Fase</TableCell>
+                                <TableCell>Fase 1</TableCell>
+                                <TableCell>Fase 2 - Expirou</TableCell>
+                                <TableCell>Fase 3 - Feedback</TableCell>
                                 <TableCell>Checkout</TableCell>
-                                <TableCell>Feedback</TableCell>
+                                <TableCell>Boleto</TableCell>
                                 <TableCell>Arquivar</TableCell>
                                 <TableCell>Excluir</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {props.transactions.map((transaction) => {
+                            {props.transactions.map((transaction, index) => {
 
                                 if (transaction.archived && !showArchived)
                                     return <TableRow key={transaction._id}></TableRow>
 
-                                let paymentType = transaction.payment_type == "PIX" ? "PIX mas não concluiu o pagamento. Teve alguma dificuldade? Posso ajudar?" : "boleto. Estou entrando em contato para te lembrar que o boleto vence hoje. Qualquer dúvida estou à disposição.";
+                                const payment_type = transaction.payment_type == "billet" ? "boleto" : transaction.payment_type
 
-                                const text = `Oi ${transaction.first_name}. Sou Alexandre do suporte da Mari Ubialli. Recebemos sua inscrição no *${transaction.prod_name}* via ${paymentType}`
+                                const phase1PaymentTypeText = transaction.payment_type == "PIX" ? "/pixajuda" : "/boletohoje";
+                                const phase1Text = `Oi ${transaction.first_name}. Recebemos sua inscrição no *${transaction.prod_name}* via ${payment_type}. ${phase1PaymentTypeText}`;
+
+                                const phase2Text = `Oi ${transaction.first_name}. ` + (transaction.payment_type == "PIX" ? "/pixexpirou" : "/boletoexpirou");
+
                                 const checkoutId = transaction.prod_name == "Curso Bonecas Joias Raras" ? "B46628840G" : "D49033705A"
                                 const checkoutUrl = `https://pay.hotmart.com/${checkoutId}?checkoutMode=10&email=${transaction.email}&name=${transaction.name}&doc=${transaction.doc}&phonenumber=${transaction.phone_checkout_number}&phoneac=${transaction.phone_checkout_local_code}`
+
                                 const feedback = `Oi ${transaction.first_name}. Entendo que você deve ter desistido da compra do *${transaction.prod_name}* e não tem problema, mas poderia compartilhar o motivo? A sua resposta é muito importante e irá ajudar a melhorarmos o suporte e a oferta dos nossos cursos. Obrigado desde já.`
+
+                                const whatsLink = `http://wa.me/55${transaction.phone_checkout_local_code}${transaction.phone_checkout_number}`;
+
+                                let rowStyle = classes.normal;
+                                switch (transaction.phase || 0) {
+                                    case 1: rowStyle = classes.phase1; break;
+                                    case 2: rowStyle = classes.phase2; break;
+                                    case 3: rowStyle = classes.phase3; break;
+                                }
 
                                 if (["expired", "waiting_payment", "canceled", "billet_printed"].includes(transaction.status)) {
                                     return (
-                                        <TableRow key={transaction._id}>
+                                        <TableRow key={transaction._id} className={rowStyle}>
                                             <TableCell>{new Date(transaction.purchase_date).toLocaleString()}</TableCell>
                                             <TableCell>{transaction.prod_name}</TableCell>
                                             <TableCell>{transaction.payment_type}</TableCell>
@@ -196,17 +246,33 @@ export default function Recupera(props) {
                                             <TableCell>{transaction.name}</TableCell>
                                             <TableCell>{transaction.email}</TableCell>
                                             <TableCell>
-                                                <a href={`http://wa.me/55${transaction.phone_local_code}${transaction.phone_number}?text=${text}`} target="_blank">
-                                                    {transaction.phone_local_code + '' + transaction.phone_number}
+                                                <a href={`http://wa.me/55${transaction.phone_local_code}${transaction.phone_number}`} target="_blank">{transaction.phone_local_code + '' + transaction.phone_number}</a><br />
+                                                <a href={whatsLink} target="_blank">{transaction.phone_checkout_local_code + '' + transaction.phone_checkout_number}</a>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Select
+                                                    value={transaction.phase || 0}
+                                                    onChange={(e) => handleChangePhase(e, e.target.value, transaction, index)}
+                                                >
+                                                    <MenuItem value={0}>0</MenuItem>
+                                                    <MenuItem value={1}>1</MenuItem>
+                                                    <MenuItem value={2}>2</MenuItem>
+                                                    <MenuItem value={3}>3</MenuItem>
+                                                </Select>
+                                            </TableCell>
+                                            <TableCell>
+                                                <a href={whatsLink + `?text=${phase1Text}`} target="_blank">
+                                                    FASE 1
                                                 </a>
                                             </TableCell>
                                             <TableCell>
-                                                <a href={`http://wa.me/55${transaction.phone_checkout_local_code}${transaction.phone_checkout_number}?text=${text}`} target="_blank">
-                                                    {transaction.phone_checkout_local_code + '' + transaction.phone_checkout_number}
+                                                <a href={whatsLink + `?text=${phase2Text}`} target="_blank">
+                                                    FASE 2 (Expirou)
                                                 </a>
                                             </TableCell>
+                                            <TableCell><a href={whatsLink + `?text=${feedback}`} target="_blank">FASE 3 (Feedback)</a></TableCell>
                                             <TableCell><a href={checkoutUrl} target="_blank">Checkout</a></TableCell>
-                                            <TableCell><a href={`http://wa.me/55${transaction.phone_checkout_local_code}${transaction.phone_checkout_number}?text=${feedback}`} target="_blank">Feedback</a></TableCell>
+                                            <TableCell>{transaction.billet_url && <a href={transaction.billet_url} target="_blank">Boleto</a>}</TableCell>
                                             <TableCell><Checkbox checked={transaction.archived} onChange={(e) => { e.preventDefault(); handleArchive(transaction) }} /></TableCell>
                                             <TableCell><a href="#" onClick={(e) => { e.preventDefault(); handleDelete(transaction) }}>Excluir</a></TableCell>
                                         </TableRow>
