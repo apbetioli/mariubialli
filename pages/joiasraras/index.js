e04465fe9bc6c44413b8d28cdabdaca619554dcd
Alexandre Betioli <apbetioli@gmail.com>
2021-05-11 15:35:22 -0300

    Google optimize activation

--- pages/joiasraras/index.js
+++ pages/joiasraras/index.js
@@ -1,5 +1,24 @@
+import React from "react";
 import JoiasRarasVendas from "./vendas";
 
-export default function JoiasRaras() {
-    return <JoiasRarasVendas />
+export default class JoiasRaras extends React.Component {
+
+    async componentDidMount() {
+        if (window.dataLayer) {
+            await window.dataLayer.push({ event: "optimize.activate" });
+            console.log("optimize.activate");
+        }
+        this.intervalId = setInterval(() => {
+            if (window.google_optimize !== undefined) {
+                const variant = window.google_optimize.get("_O3ZF-sVRhWDUSE3AV_xjA");
+                console.log("Optimize variant " + variant);
+                this.setState({ variant });
+                clearInterval(this.intervalId);
+            }
+        }, 100);
+    }
+
+    render() {
+        return <JoiasRarasVendas />
+    }
 }
