90126b746c16a1938e0eceb2c0fa22dfa1560b7c
Alexandre Betioli <apbetioli@gmail.com>
2021-05-21 10:41:55 -0300

    WIP

--- pages/joiasraras/vendas.js
+++ pages/joiasraras/vendas.js
@@ -17,6 +17,7 @@ import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import Head from "next/head";
 import { default as React } from "react";
+import useSWR from "swr";
 import BotaoWhats from "../../components/BotaoWhats";
 import ColorButton from "../../components/ColorButton";
 import Footer from "../../components/Footer";
@@ -633,6 +634,8 @@ function Testemunhos3() {
 }
 
 export default function JoiasRarasVendas(props) {
+  const { data, error } = useSWR('/api/fbevent', fetch)
+
   const classes = useStyles();
   return (
     <main className={classes.root}>
