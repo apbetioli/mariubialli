11667ae720672d16c85e689fac3e3ff56e3f511a
Alexandre Betioli <apbetioli@gmail.com>
2021-09-15 23:25:44 -0300

    Update master from ls1

--- next.config.js
+++ next.config.js
@@ -12,64 +12,9 @@ module.exports = withPlugins([[withImages]], {
     return [
       {
         source: '/',
-        destination: '/ac/espera',
+        destination: '/cursos',
         permanent: false,
-      },
-      {
-        source: '/inscrever/:origin*',
-        destination: '/ac/espera',
-        permanent: false,
-      },
-      {
-        source: '/obrigado/:origin*',
-        destination: '/ac/espera',
-        permanent: false,
-      },
-      {
-        source: '/replay',
-        destination: '/ac/espera',
-        permanent: false,
-      },
-      {
-        source: '/confirmado',
-        destination: '/ls1/confirmado',
-        permanent: false,
-      },
-      {
-        source: '/aguardando',
-        destination: '/ls1/aguardando',
-        permanent: false,
-      },
-      {
-        source: '/whats',
-        destination: 'https://chat.whatsapp.com/FrpF2gtQmRt1YSAiY7EwaP',
-        permanent: false,
-      },
-      {
-        source: '/telegram',
-        destination: 'https://t.me/mariubialli',
-        permanent: false,
-      },
-      {
-        source: '/aula',
-        destination: '/ac/espera',
-        permanent: false,
-      },
-      {
-        source: '/mm',
-        destination: '/ac/espera',
-        permanent: false,
-      },
-      {
-        source: '/checkout',
-        destination: 'https://sun.eduzz.com/969161',
-        permanent: false
-      },
-      {
-        source: '/desafio',
-        destination: '/ac/espera',
-        permanent: false,
-      },
+      }
     ]
   },
 });
