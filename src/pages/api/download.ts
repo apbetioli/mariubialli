4f0f6b2996fbdeb5ad786f7dcf30f6fbe2def345
Alexandre Betioli <apbetioli@gmail.com>
2022-03-11 18:40:58 -0300

    Redirect logged in to /apostilas

--- src/pages/api/download.ts
+++ src/pages/api/download.ts
@@ -2,10 +2,6 @@ import { getSession } from "next-auth/react";
 import apostilaQuery from "../../graphql/queries/apostila.graphql";
 import client from "../../lib/graphqlClient";
 
-function fullUrl(req) {
-  return "http://" + req.headers.host + req.url;
-}
-
 export default async function handler(req, res) {
   if (!req.query.slug) return res.status(404).end();
 
@@ -21,6 +17,8 @@ export default async function handler(req, res) {
 
     return res.redirect(`${data.apostila.download.url}`);
   } else {
-    return res.redirect(`/api/auth/signin?callbackUrl=${fullUrl(req)}`);
+    return res.redirect(
+      `/api/auth/signin?callbackUrl=http://${req.headers.host}/apostilas`
+    );
   }
 }
