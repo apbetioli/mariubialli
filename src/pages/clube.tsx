56c72a85b98913ca03dcbbe12ff5368791a6d51b
Alexandre Betioli <apbetioli@gmail.com>
2022-03-22 14:49:16 -0300

    Add SEO component

--- src/pages/clube.tsx
+++ src/pages/clube.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Section from "../components/Section";
+import SEO from "../components/SEO";
 import WhatsAppButton from "../components/WhatsAppButton";
 import salesPageQuery from "../graphql/queries/salesPage.graphql";
 import client from "../lib/graphqlClient";
 
-export default function Clube({ hero, sections, whatsApp }) {
+export default function Clube({ seo, hero, sections, whatsApp }) {
   return (
     <>
+      {seo && <SEO {...seo} />}
+
       <Hero {...hero} />
 
       {sections.map((section, index) => {
