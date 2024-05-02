ba41963287bbd61682c55a20a863e1cc5cb15270
Alexandre Betioli <apbetioli@gmail.com>
2024-05-02 18:40:48 +0200

    Improve seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -46,6 +46,68 @@ const courseData: Prisma.CourseCreateInput[] = [
       },
     },
   },
+  {
+    name: 'Iniciantes em feltro',
+    slug: kebabCase('Iniciantes em feltro'),
+    description: 'Aulas para iniciantes em feltro.',
+    image: 'https://media.graphassets.com/dsoPcycSP23gtJQMIK4i',
+    groups: {
+      create: [
+        {
+          name: 'Aulas',
+          lessons: {
+            create: [
+              {
+                name: 'Riscar e cortar o feltro',
+                slug: kebabCase('Riscar e cortar o feltro'),
+                video: 'https://youtu.be/PCTICFNt2Os',
+              },
+              {
+                name: 'Dica para cortar peças pequenas',
+                slug: kebabCase('Dica para cortar peças pequenas'),
+                video: 'https://youtu.be/XVXj3iREBUs',
+              },
+              {
+                name: 'Dica para cortar peças pequenas 2',
+                slug: kebabCase('Dica para cortar peças pequenas 2'),
+                video: 'https://youtu.be/cuCF3_tJGGM',
+              },
+              {
+                name: 'Como cortar moldes espelhados',
+                slug: kebabCase('Como cortar moldes espelhados'),
+                video: 'https://youtu.be/K2w_kYcf7Wo',
+              },
+              {
+                name: 'Como usar os gabaritos',
+                slug: kebabCase('Como usar os gabaritos'),
+                video: 'https://youtu.be/r-Nw0KfdG-A',
+              },
+              {
+                name: 'Como fazer ponto atrás',
+                slug: kebabCase('Como fazer ponto atrás'),
+                video: 'https://youtu.be/tlHJ0vrpcp8',
+              },
+              {
+                name: 'Como fazer ponto caseado',
+                slug: kebabCase('Como fazer ponto caseado'),
+                video: 'https://youtu.be/H7ZUclfa7rs',
+              },
+              {
+                name: 'Como fazer ponto palito',
+                slug: kebabCase('Como fazer ponto palito'),
+                video: 'https://youtu.be/CDUQbqA47-0',
+              },
+              {
+                name: 'Como fazer nó francês',
+                slug: kebabCase('Como fazer nó francês'),
+                video: 'https://youtu.be/U8N3X5tjRE8',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
 ]
 
 async function main() {
