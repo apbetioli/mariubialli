c7ce8ad7f160133491bdb66650fb58b1693bcbfc
Alexandre Betioli <apbetioli@gmail.com>
2024-05-02 18:28:28 +0200

    Update prisma model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { kebabCase } from 'lodash'
 
 const prisma = new PrismaClient()
 
@@ -10,8 +11,46 @@ const userData: Prisma.UserCreateInput[] = [
   },
 ]
 
+const courseData: Prisma.CourseCreateInput[] = [
+  {
+    name: 'Quadro - Dia das Mães',
+    slug: kebabCase('Quadro - Dia das Mães'),
+    description:
+      'Aprenda a fazer um quadro em feltro para decorar seu ateliê, presentear sua mãe ou se presentear.',
+    image: 'https://media.graphassets.com/13BiFOg7REOVUW1WkKMG',
+    groups: {
+      create: [
+        {
+          name: 'Aulas',
+          lessons: {
+            create: [
+              {
+                name: 'Aula Quadro Dia das Mães',
+                slug: kebabCase('Aula Quadro Dia das Mães'),
+                video: 'https://youtu.be/jX0vFKPB4SU',
+              },
+            ],
+          },
+        },
+      ],
+    },
+    asset: {
+      create: {
+        name: 'Apostila de moldes',
+        description:
+          'Apostila de moldes digital contendo todos os elementos necessários para criar o quadro em feltro com o tema dia das mães.',
+        image:
+          'https://mariubialli.s3.amazonaws.com/Imagens/asset-quadro-dia-das-maes.jpg',
+        price: 0,
+        filename: 'Mari Ubialli - Álbum Dia das Mães.pdf',
+      },
+    },
+  },
+]
+
 async function main() {
   console.log(`Start seeding ...`)
+
   for (const data of userData) {
     const user = await prisma.user.upsert({
       create: data,
@@ -20,6 +59,16 @@ async function main() {
     })
     console.log(`Created/Updated user with id: ${user.id}`)
   }
+
+  for (const data of courseData) {
+    const course = await prisma.course.upsert({
+      create: data,
+      update: data,
+      where: { slug: data.slug },
+    })
+    console.log(`Created/Updated course with id: ${course.id}`)
+  }
+
   console.log(`Seeding finished.`)
 }
 
