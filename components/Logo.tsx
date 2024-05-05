450f23fec54fb65cf765cf6895111c02d36dca82
Alexandre Betioli <apbetioli@gmail.com>
2024-05-05 22:57:16 +0200

    Add logo image

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,22 @@
 'use client'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 
-export function Logo({ className }: { className?: string }) {
+export function Logo({
+  className,
+  onClick,
+}: {
+  className?: string
+  onClick?: () => void
+}) {
   return (
-    <Link className={cn('font-bold', className)} href="/">
+    <Link
+      className={cn('font-bold flex items-center gap-2', className)}
+      href="/"
+      onClick={onClick}
+    >
+      <Image src="/logo.webp" width={30} height={30} alt="Logo" />
       MariFLIX
     </Link>
   )
