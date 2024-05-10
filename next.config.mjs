bfa42e4ee2c5c4a86bf629cacbbbc1d890e6752e
Alexandre Betioli <apbetioli@gmail.com>
2024-05-10 21:50:15 +0200

    Add cors to next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,31 @@ import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //https://vercel.com/guides/how-to-enable-cors#enabling-cors-in-a-next.js-app
+  async headers() {
+    return [
+      {
+        source: '/api/stripe/webhook*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
         hostname: 'mariubialli.s3.amazonaws.com',
-      },
-      {
-        hostname: 'images.unsplash.com',
       },
       {
         hostname: 'media.graphassets.com',
