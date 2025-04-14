f1cb592c051540802514c0f730ef5ed1b8d6777a
Alexandre Betioli <alexandre.betioli@beatgridmedia.com>
2025-04-14 19:22:30 +0200

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath } from 'url'
-import { defineConfig } from 'vitest/config'
+import { defineConfig, UserConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as UserConfig["plugins"],
   test: {
     environment: 'happy-dom',
     mockReset: true,
